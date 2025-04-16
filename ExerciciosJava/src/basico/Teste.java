839ee5aad105fa55fb4ca4711ac082d83eb32d1a
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2025-04-16 15:34:05 -0300

    criacao de estrutura Java

---
+++ ExerciciosJava/src/basico/Teste.java
@@ -0,0 +1,9 @@
+package basico;
+
+// Escreva um programa que imprima "Hello, World!" no console.
+
+public class Teste {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
